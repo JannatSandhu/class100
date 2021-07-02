d91c10b5500f03549a7c817291fe88593212b503
JannatSandhu <67374324+JannatSandhu@users.noreply.github.com>
2021-07-02 16:17:25 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,8 @@
+class atm():
+    def __init__(self,CashWithdrawl,BalanceEnquiry):
+        self.CashWithdrawl= CashWithdrawl
+        self.BalanceEnquiry= BalanceEnquiry
+    
+    def printCashWithdrawl(self):
+        print(self.CashWithdrawl)
+
